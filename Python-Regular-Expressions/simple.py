96cd7333404ed33f84261ca47a9b3772c78b4217
Kelly Gilbert <gilbert.kelly.lynn@gmail.com>
2019-07-12 17:39:14 -0400

    updated Python-Regular-Expressions with notes from video

--- Python-Regular-Expressions/simple.py
+++ Python-Regular-Expressions/simple.py
@@ -27,6 +27,112 @@ Mr. T
 
 sentence = 'Start a sentence and then bring it to an end'
 
+
+# re.compile allows you to save the pattern to a variable
+# r'string' is a raw string (literal interpretation)
+# this is important because you may need to use backslashes to escape
+#pattern = re.compile(r'\.')
+pattern = re.compile(r'\b')
+
+# matches is an iterator that contains all of the matches of pattern
+matches = pattern.finditer(text_to_search)
+
+# iterate through the matches
+for match in matches:
+    print(match)
+    
+
+print(text_to_search[1:4])
+
+
+
+# word boundaries
+text_to_search2 = 'abc def ghijkl '
+pattern = re.compile(r'\b')
+matches = pattern.finditer(text_to_search2)
+
+for match in matches:
+    print(match)
+ 
+    
+# match phone numbers
+pattern = re.compile('\d{3}.\d{3}.\d{4}')    
+matches = pattern.finditer(text_to_search)
+for match in matches:
+    print(match)
+
+
+# read in a file of contact info and find all of the phone numbers
+with open('data.txt', 'r') as f:
+    contents = f.read()
+    
+pattern = re.compile('\d{3}.\d{3}.\d{4}')    
+matches = pattern.finditer(contents)
+for match in matches:
+    print(match)
+    
+
+# only match a specific character set (dash or a dot between numbers)
+pattern = re.compile('\d{3}[-.]\d{3}[-.]\d{4}')    
+matches = pattern.finditer(contents)
+for match in matches:
+    print(match)
+
+
+# only match 800- and 900- numbers
+pattern = re.compile('[89]00[-.]\d{3}[-.]\d{4}')    
+matches = pattern.finditer(contents)
+for match in matches:
+    print(match)
+
+
+
+# only match numbers 1 through 5
+pattern = re.compile('[1-5]')    
+matches = pattern.finditer(contents)
+for match in matches:
+    print(match)
+
+
+# only match numbers ucase or lcase a-e
+pattern = re.compile('[A-Ea-e]')    
+matches = pattern.finditer(contents)
+for match in matches:
+    print(match)
+
+
+# findall method (similar to finditer, except it just returns the matching text
+# and not a match object
+pattern = re.compile('\d{3}.\d{3}.\d{4}')    
+matches = pattern.findall(contents)
+for match in matches:
+    print(match)
+
+
+# match method: finds a match at the start of the string and returns 
+# the match object
+# (or None if there is no match)
+pattern = re.compile(r'Start')    
+matches = pattern.match(sentence)
+print(matches)
+
+
+# search method: same as match, except finds first match anywhere within the string
+# (or None if there is no match)
+pattern = re.compile(r'1234')    
+matches = pattern.search(text_to_search)
+print(matches)
+
+
+# flags
+pattern = re.compile(r's', re.IGNORECASE)    
+matches = pattern.finditer(sentence)
+for match in matches:
+    print(match)
+
+
+
+
 pattern = re.compile(r'start', re.I)
 
 matches = pattern.search(sentence)
--- Python-Regular-Expressions/urls.py
+++ Python-Regular-Expressions/urls.py
@@ -7,13 +7,32 @@ https://youtube.com
 https://www.nasa.gov
 '''
 
-pattern = re.compile(r'https?://(www\.)?(\w+)(\.\w+)')
 
+# match urls
+pattern = re.compile('https?://(www\.)?\w+\.\w+')    
+matches = pattern.finditer(urls)
+for match in matches:
+    print(match)
+
+
+# capture groups
+# 1 = optional www, 2 = domain, 3 = top-level domain
+pattern = re.compile('https?://(www\.)?(\w+)(\.\w+)')    
+matches = pattern.finditer(urls)
+for match in matches:
+    print(match.group(1), match.group(2), match.group(3), sep='\t')
+
+
+# use capture groups to substitute group2+group3 for the full url
+# i.e. remove the optional www
 subbed_urls = pattern.sub(r'\2\3', urls)
-
 print(subbed_urls)
 
-# matches = pattern.finditer(urls)
 
-# for match in matches:
-#     print(match.group(3))
+
+# capture groups
+# 1 = optional www, 2 = domain, 3 = top-level domain
+pattern = re.compile('https?://(www\.)?(\w+)(\.\w+)')    
+matches = pattern.finditer(urls)
+for match in matches:
+    print(match.group(1), match.group(2), match.group(3), sep='\t')
